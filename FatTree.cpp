f8a1886e67238939db4488332bfca4d6ffbac121
om3g4zell <louis.babuchon@etu.univ-tours.fr>
2018-03-03 00:58:28 +0100

    Ecriture dans le fichier

--- FatTree.cpp
+++ FatTree.cpp
@@ -1,6 +1,7 @@
 #include "FatTree.hpp"
 
 #include <iostream>
+#include <fstream>
 #include <cmath>
 
 FatTree::FatTree(unsigned int numberOfPods)
@@ -35,7 +36,7 @@ FatTree::~FatTree()
  * @brief Helper function to print node links.
  * @param a reference to a node
  */
-void printNodeLinks(Node& n)
+void printNodeLinks(Node& n, std::ostream& stream)
 {
     unsigned int j(0);
     for(NodeLink& nl : n.getLinks())
@@ -45,24 +46,36 @@ void printNodeLinks(Node& n)
             continue;
 
        // std::cout << "nl.node: " << nl.node << std::endl;
-        std::cout << "[" << j << "] \"" << nl.node->getName() << "\"[" << nl.portNumber << "]" << std::endl;
+        stream << "[" << j << "] \"" << nl.node->getName() << "\"[" << nl.portNumber << "]" << std::endl;
     }
-    std::cout << std::endl;
+    stream << std::endl;
 }
 
 void FatTree::serialize(const std::string& filepath)
 {
-    std::cout << "Listing..." << std::endl;
+
+    std::ofstream file(filepath);
+    if(!file)
+    {
+        std::cout << "Erreur lors de l'ouverture du fichier";
+        return ;
+    }
+
+    std::cout << "writting" << std::endl;
+    file << "#Fat tree topology file." << std::endl << "#Value of k = " << m_numberOfPods << std::endl;
+    file << "#Total number of hosts = " << (m_numberOfPods * (std::pow(m_numberOfPods / 2, 2))) << std::endl;
+    file << "#Number of hosts under each switch = " << m_numberOfPods / 2 << std::endl;
+    file << "############################################" << std::endl << std::endl;
 
     // List the core nodes.
     for(unsigned int i(0) ; i < m_cores.size() ; ++i)
     {
         std::string hw = (m_cores[i]->getHardware() == NodeHardware::Hca) ? "Hca" : "Switch";
-        std::cout << hw << "\t" << m_cores[i]->getNumberOfPorts() << "\t\"" << m_cores[i]->getName() << "\"" << std::endl;
-        printNodeLinks(*m_cores[i]);
+        file << hw << "\t" << m_cores[i]->getNumberOfPorts() << "\t\"" << m_cores[i]->getName() << "\"" << std::endl;
+        printNodeLinks(*m_cores[i], file);
     }
 
-    std::cout << std::endl;
+    file << std::endl;
 
     // List the edge + aggregation.
     for(unsigned int i(0) ; i < m_pods.size() ; ++i)
@@ -70,21 +83,21 @@ void FatTree::serialize(const std::string& filepath)
         for(Node * n : m_pods[i]->getEdge())
         {
             std::string hw = (n->getHardware() == NodeHardware::Hca) ? "Hca" : "Switch";
-            std::cout << hw << "\t" << n->getNumberOfPorts() << "\t\"" << n->getName() << "\"" << std::endl;
-            printNodeLinks(*n);
+            file << hw << "\t" << n->getNumberOfPorts() << "\t\"" << n->getName() << "\"" << std::endl;
+            printNodeLinks(*n, file);
         }
 
-        std::cout << std::endl;
+        file << std::endl;
 
         for(Node * n : m_pods[i]->getAggregation())
         {
             std::string hw = (n->getHardware() == NodeHardware::Hca) ? "Hca" : "Switch";
-            std::cout << hw << "\t" << n->getNumberOfPorts() << "\t\"" << n->getName() << "\"" << std::endl;
-            printNodeLinks(*n);
+            file << hw << "\t" << n->getNumberOfPorts() << "\t\"" << n->getName() << "\"" << std::endl;
+            printNodeLinks(*n, file);
         }
     }
 
-    std::cout << std::endl;
+    file << std::endl;
 
     // List the workstations.
     for(unsigned int i(0) ; i < m_pods.size() ; ++i)
@@ -92,8 +105,9 @@ void FatTree::serialize(const std::string& filepath)
         for(Node * n : m_pods[i]->getWorkstation())
         {
             std::string hw = (n->getHardware() == NodeHardware::Hca) ? "Hca" : "Switch";
-            std::cout << hw << "\t" << n->getNumberOfPorts() << "\t\"" << n->getName() << "\"" << std::endl;
-            printNodeLinks(*n);
+            file << hw << "\t" << n->getNumberOfPorts() << "\t\"" << n->getName() << "\"" << std::endl;
+            printNodeLinks(*n, file);
         }
     }
+    std::cout << "succes" << std::endl;
 }
